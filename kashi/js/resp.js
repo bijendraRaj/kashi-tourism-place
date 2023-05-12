83841dfc72908cdbc1cde50e26cd2c49dcf9d92a
Bijendra Raj <94780388+bijendraRaj@users.noreply.github.com>
2023-05-12 15:09:03 +0530

    Add files via upload

---
+++ kashi/js/resp.js
@@ -0,0 +1,11 @@
+burger =  document.querySelector('.burger')
+navbar =  document.querySelector('.navbar')
+navlist =  document.querySelector('.nav-list')
+rightnav =  document.querySelector('.rightna')
+
+
+
+
+
+
+
